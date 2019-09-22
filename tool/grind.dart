bbb9cd2879d1b71fcec12e10b88db85c10fce578
Cédric Belin <cedric@belin.io>
2019-09-22 03:56:37 +0200

    Added the build system

---
+++ tool/grind.dart
@@ -0,0 +1,45 @@
+import 'package:grinder/grinder.dart';
+
+/// Starts the build system.
+Future<void> main(List<String> args) => grind(args);
+
+@DefaultTask('Builds the project')
+void build() => Pub.run('build_runner', arguments: ['build', '--delete-conflicting-outputs']);
+
+@Task('Deletes all generated files and reset any saved state')
+void clean() {
+  defaultClean();
+  ['.dart_tool', 'doc/api', webDir.path].map(getDir).forEach(delete);
+}
+
+@Task('Builds the documentation')
+Future<void> doc() async {
+  for (final path in ['CHANGELOG.md', 'LICENSE.md']) await getFile(path).copy('doc/about/${path.toLowerCase()}');
+  DartDoc.doc();
+  run('mkdocs', arguments: ['build', '--config-file=doc/mkdocs.yaml']);
+  ['doc/about/changelog.md', 'doc/about/license.md', '${webDir.path}/mkdocs.yaml'].map(getFile).forEach(delete);
+}
+
+@Task('Fixes the coding standards issues')
+void fix() => DartFmt.format(existingSourceDirs);
+
+@Task('Performs the static analysis of source code')
+void lint() => Analyzer.analyze(existingSourceDirs);
+
+@Task('Publishes the package to the registry')
+@Depends(clean, fix)
+void publish() => run('pub', arguments: ['publish', '--force']);
+
+@Task('Runs the test suites')
+void test() => Pub.run('build_runner', arguments: ['test', '--delete-conflicting-outputs']);
+
+@Task('Upgrades the project to the latest revision')
+void upgrade() {
+  run('git', arguments: ['reset', '--hard']);
+  run('git', arguments: ['fetch', '--all', '--prune']);
+  run('git', arguments: ['pull', '--rebase']);
+  Pub.upgrade();
+}
+
+@Task('Watches for file changes')
+void watch() => Pub.run('build_runner', arguments: ['watch', '--delete-conflicting-outputs']);
