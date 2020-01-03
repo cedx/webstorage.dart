60d6cc8ee2581c454ebeb495e04b02d531976d30
Cédric Belin <cedric@belin.io>
2020-01-03 18:17:38 +0100

    Updated the generated files

--- lib/webstorage.g.dart
+++ lib/webstorage.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'webstorage.dart';
+part of webstorage;
 
 // **************************************************************************
 // JsonSerializableGenerator
